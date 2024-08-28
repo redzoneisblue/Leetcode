693c8aa35f24d2b353c3245e2ac27e2710854be4
redzoneisblue <109468308+redzoneisblue@users.noreply.github.com>
2024-08-28 19:16:30 +0530

    done

--- 9_Palindrome.cpp
+++ 9_Palindrome.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-   cout << "Hello World"; // prints Hello World
-};
+bool isPalindrome(int n){
+   int original = n;
+   int reversed = 0;
+   while(n>0) {
+      int digit = n%10;
+      reversed = reversed * 10 + digit;
+      n /= 10;
+   }
+   return original == reversed ;
+}
+
+int main() {
+   int num;
+   cout << "Enter a number :";
+   cin >> num;
+
+   if (isPalindrome(num)) {
+      cout << "true" <<endl;
+   }
+   else {
+      cout<<num << "false" << endl;
+   }
+   return 0;
+}
